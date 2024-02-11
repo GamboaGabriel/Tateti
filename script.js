9f4cb226ee7d3d31320b25c353571100bffcf372
Gabriel Gamboa <maxisjrc@gmail.com>
2024-02-11 05:28:13 -0300

    2do commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-function realizarMovimiento(indice) {
+function realizarMovimiento(indice) {""
     if (tablero[indice] === '') {
         tablero[indice] = jugadorActual;
         renderizarTablero();
